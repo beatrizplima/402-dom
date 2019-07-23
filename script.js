a331f8fab24d76d32e3bf595ed038d6eacaefab6
Beatriz Pavan de Lima <bia.pavan1904@gmail.com>
2019-07-23 17:59:43 -0300

    Realizar atividade extra

--- script.js
+++ script.js
@@ -1,45 +1,18 @@
-//function somar (num1, num2){
-//    console.log(num1 + num2);
-//}
-
-//somar(57, 32);
-
-
-//function subtrair (num1, num2){
-//    console.log(num1 - num2);
-//}
-
-//subtrair(690, 123);
-
-//let corpo = document.querySelector("body");
-//let botao = document.querySelector("button");
-
-//function trocarCor(){
-  //  corpo.style.backgroundColor = "limegreen";
-//}
-
-//botao.onclick = trocarCor;
-
-//let corpo = document.querySelector("body");
-//let botoes = document.querySelectorAll("button");
+let emails = [
+    {email: "paula.18@gmail.com", senha:"123"},
+    {email: "joao@gmail.com", senha:"jujubinha12"},
+    {email: "be.nave@seilaoq.com", senha: "789"},
+    {email: "fofurinha13@gmail.com", senha: "456"},
+    {email: "beeb@gmail.com", senha: "147"},
+];
 
 let inputEmail = document.querySelector("#inputEmail");
 let inputSenha = document.querySelector("#inputSenha");
 let resposta = document.querySelector("p");
-let botao = document.querySelector("button");
 
-function validarLogin(){
-    if(inputEmail.value == "bia.pavan1904@gmail.com" && inputSenha.value == "batatinha"){
-        resposta.innerHTML = "Login Aprovado";
-        resposta.style.color = "blue";
-        resposta.style.border = "2px blue solid";
-    }
-
-    else{
-        resposta.innerHTML = "Login Inválido";
-        resposta.style.color = "red";
-        resposta.style.border = "2px red dotted";
+function validarLogin();
+for(let contas of emails){
+    if(inputEmail.value == emails.contas && inputSenha.value == emails.senha){
+        resposta.innerHTML = "Login Aprovado"
     }
 }
-
-botao.onclick = validarLogin;
